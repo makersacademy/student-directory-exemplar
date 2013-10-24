d8a01783d4c57379cb95160456fc830d45a42b55
Evgeny Shadchnev <evgeny.shadchnev@gmail.com>
2013-10-24 14:00:38 +0100

    Extracted most code into three methods

--- directory.rb
+++ directory.rb
@@ -10,11 +10,22 @@ students = [
   "Freddy Kruger",
   "The Joker"
 ]
-# and then print them
-puts "The students of my cohort at Makers Academy"
-puts "-------------"
-students.each do |student|
-  puts student
+
+def print_header
+  puts "The students of my cohort at Makers Academy"
+  puts "-------------"
 end
-# finally, we print the total
-puts "Overall, we have #{students.length} great students"
+
+def print(names)
+  names.each do |name|
+    puts name
+  end
+end
+
+def print_footer(names)    
+  puts "Overall, we have #{names.length} great students"
+end
+
+print_header
+print(students)
+print_footer(students)
