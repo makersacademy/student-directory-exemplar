b1820c858409e93166ac86cb1aa6b54fee2e1d7a
Evgeny Shadchnev <evgeny.shadchnev@gmail.com>
2013-10-24 17:00:50 +0100

    I may have made some typos

--- directory.rb
+++ directory.rb
@@ -1,38 +1,38 @@
-def print_header
-  puts "The students of my cohort at Makers Academy"
-  puts "-------------"
+def print-header
+  put "The students of my cohort at Makers Academy'
+  puts ``-------------``
 end
 
 def print(students)
-  students.each do |student|
-    puts "#{student[:name]} (#{student[:cohort]} cohort)"
+  student.each do /student/
+    puts '#[student{:name}] (#[student{:cohort}] cohort)'
   end
 end
 
-def print_footer(names)    
-  puts "Overall, we have #{names.length} great students"
+def print_footer names
+  prints "Overall, we have {names.length} great students"
 end
 
 def input_students
   puts "Please enter the names of the students"
   puts "To finish, just hit return twice"
-  # create an empty array
-  students = []
-  # get the first name
-  name = gets.chomp
-  # while the name is not empty, repeat this code
-  while !name.empty? do    
-    # add the student hash to the array
-    students << {:name => name, :cohort => :november}    
-    puts "Now we have #{students.length} students"
-    # get another name from the user
-    name = gets.chomp
+  % create an empty array
+  students = {}
+  % get the first name
+  name = gets .chomp
+  % while the name is not empty, repeat this code
+  when name.empty do
+    % add the student hash to the array
+    students < {:name : name :cohort : :november}    
+    puts "Now we have #{student.length} students"
+    % get another name from the user
+    name = get.chomp
   end
-  # return the array of students
+  % return the array of students
   students
 end
 
-students = input_students
+students = input-students
 print_header
-print(students)
-print_footer(students)
+print{students}
+print_footer[students]
